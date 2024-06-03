9b742dc6bb27d8dc15380428eae8042f3d6ab475
matthew4np <matthew.ding@gmail.com>
2024-06-03 21:16:18 +0800

    testing

--- PF1-Assignment/script.js
+++ PF1-Assignment/script.js
@@ -1,41 +1,41 @@
 const Obj = {
     burger: [
-      {name:"Chicken Burger", price: 5, quantity: 0},
-      {name:"Beef Burger", price: 6, quantity: 0},
-      {name:"Fish Burger", price: 6, quantity: 0}
+      {name:"Chicken Burger", price: 5, quantity: 1},
+      {name:"Beef Burger", price: 6, quantity: 1},
+      {name:"Fish Burger", price: 6, quantity: 1}
     ]
 }
 
 const orderTotal = {
     totalPrice: 0,
-    orderBurgerItem: [{}],
+    orderBurgerItem: [],
 
-    AddBurger: function(){
-        // this.orderBurgerItem[this.orderBurgerItem.length] = {
-        //     name: n,
-        //     price: p,
-        //     quantity: q
-        // }
-        alert("hello");
-        //document.getElementsByClassName("cost").item(0).innerHTML = `<p>` + n + `</p>`; //orderTotal.orderBurgerItem.length;
+    AddBurger: function(n){
+        orderTotal.orderBurgerItem.push({
+            name: Obj.burger[n].name,
+            price: Obj.burger[n].price,
+            quantity: Obj.burger[n].quantity
+        })
+        this.totalPrice = this.totalPrice + Obj.burger[n].price;
+        document.getElementsByClassName("cost").item(0).innerHTML = JSON.stringify(orderTotal);
     },
 
     //Use the button Minus to remove the burger from the order burger array
     RemoveBurger: function(n){
-        //find the index number of the chicken burger
-        let i = this.orderBurgerItem.findIndex(n);
-        //Use the splice function to delete the burger item
-        this.orderBurgerItems.splice(i, 1);
-        
-    },
-
-    DisplayOrderTotal : function(){
-        let text = "";
-        for (let i = 0; i < orderTotal.orderItem.length; i++)
-        {
-          text += "<h3>" + orderTotal.orderItem[i].orderName + " " + orderTotal.orderItem[i].orderPrice + " " + orderTotal.orderItem[i].orderQuantity  + "<br>" + "</h3>";
+        let h = Obj.burger[n].name
+        let myArr = orderTotal.orderBurgerItem.map(myArrFunction)
+        function myArrFunction(myObject){
+            return myObject.name
         }
-        return text;
+        let i = myArr.indexOf(h)
+        if (i == -1)
+        {
+            document.getElementsByClassName("cost").item(0).innerHTML = JSON.stringify(orderTotal)
+        } else
+        {
+            orderTotal.orderBurgerItem.splice(i, 1)
+            document.getElementsByClassName("cost").item(0).innerHTML = JSON.stringify(orderTotal)
+        }
     },
 
     DisplayBurgerItem : function() {
@@ -44,10 +44,10 @@ const orderTotal = {
         {
             let j = `\"${Obj.burger[i].name}\", ${Obj.burger[i].price}, ${Obj.burger[i].quantity}`;
     
-            text += `<h3 class=\"burgers-item\">` + Obj.burger[i].name + " " + Obj.burger[i].price + " " + Obj.burger[i].quantity + `<br>` + `<button type=\"button\" onclick=\"orderTotal.AddBurger(Obj.burger[i])\">+</button>` + `<button type=\"button\">` + Obj.burger[i].quantity + `</button>` + `<button type=\"button\" onclick=\"orderTotal.RemoveBurger(Obj.burger[i])\">-</button>` + `<br>` + `</h3>`;
+            text += `<h3 class=\"burgers-item\">` + Obj.burger[i].name + " " + Obj.burger[i].price + " " + Obj.burger[i].quantity + `<br>` + `<button type=\"button\" onclick=\"orderTotal.AddBurger(${i})\">+</button>` + `<button type=\"button\">` + Obj.burger[i].quantity + `</button>` + `<button type=\"button\" onclick=\"orderTotal.RemoveBurger(${i})\">-</button>` + `<br>` + `</h3>`;
         }
         return text;
     }
 }
-//console.log(orderTotal.DisplayBurgerItem())
+//console.log(orderTotal.DisplayBurgerItem());
 document.getElementsByClassName("burgerchoice").item(0).innerHTML = orderTotal.DisplayBurgerItem();
